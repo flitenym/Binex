172fed7330430e8e88fa1d75c134b62b94f01402
flitenym <flitenym@gmail.com>
2021-08-01 18:06:23 +0500

    resolve

--- binex/Helper/StaticInfo/StaticClass.cs
+++ binex/Helper/StaticInfo/StaticClass.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Binex.Helper.StaticInfo
+﻿namespace Binex.Helper.StaticInfo
 {
     public static class StaticClass
     {
-        public const string BTC => "BTC";
-        public const string USDT => "USDT";
+        public const string BTC = "BTC";
+        public const string USDT = "USDT";
     }
 }
