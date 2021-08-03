406f53f204fd0be144ff338a317eb4c291e7cabc
EOSPF\adolmatkin <dolmatkin@eosplatforma.ru>
2021-08-03 16:25:09 +0500

    Основное для создание лицензии

---
+++ LicenseGenerator/LicenseTerm.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace LicenseGenerator
+{
+    /// terms of the license agreement: it&#39;s not encrypted (but is obscured)
+    /// </summary>
+    [Serializable]
+    internal class LicenseTerms
+    {
+        /// <summary>
+        /// start date of the license agreement.
+        /// </summary>
+        public DateTime StartDate { get; set; }
+
+        /// <summary>
+        /// registered user name for the license agreement.
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// the assembly name of the product that is licensed.
+        /// </summary>
+        public string ProductName { get; set; }
+
+        /// <summary>
+        /// the last date on which the software can be used on this license.
+        /// </summary>
+        public int Month { get; set; }
+
+        /// <summary>
+        /// returns the license terms as an obscure (not human readable) string.
+        /// </summary>
+        /// <returns></returns>
+        public string GetLicenseString()
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                // create a binary formatter:
+                BinaryFormatter bnfmt = new BinaryFormatter();
+
+                // serialize the data to the memory-steam;
+                bnfmt.Serialize(ms, this);
+
+                // return a base64 string representation of the binary data:
+                return Convert.ToBase64String(ms.GetBuffer());
+
+            }
+        }
+
+        /// <summary>
+        /// returns a binary representation of the license terms.
+        /// </summary>
+        /// <returns></returns>
+        public byte[] GetLicenseData()
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                // create a binary formatter:
+                BinaryFormatter bnfmt = new BinaryFormatter();
+
+                // serialize the data to the memory-steam;
+                bnfmt.Serialize(ms, this);
+
+                // return a base64 string representation of the binary data:
+                return ms.GetBuffer();
+
+            }
+        }
+
+        /// <summary>
+        /// create a new license-terms object from a string-representation of the binary
+        /// serialization of the licence-terms.
+        /// </summary>
+        /// <param name="licenseTerms"></param>
+        /// <returns></returns>
+        internal static LicenseTerms FromString(string licenseTerms)
+        {
+            using (MemoryStream ms = new MemoryStream(Convert.FromBase64String(licenseTerms)))
+            {
+                // create a binary formatter:
+                BinaryFormatter bnfmt = new BinaryFormatter();
+
+                // serialize the data to the memory-steam;
+                object value = bnfmt.Deserialize(ms);
+
+                if (value is LicenseTerms)
+                    return (LicenseTerms)value;
+                else
+                    throw new ApplicationException("Invalid Type!");
+            }
+        }
+    }
+}
--- LicenseGenerator/Program.cs
+++ LicenseGenerator/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Security.Cryptography;
-using System.Xml;
 
 namespace LicenseGenerator
 {
@@ -11,26 +9,11 @@ namespace LicenseGenerator
             bool IsEnd = false;
 
             while (!IsEnd) {
-                var result = License();
+                var result = GetLicenseData();
                 if (result.IsSuccess)
                 {
-                    MD5 md5 = new MD5CryptoServiceProvider();
-                    byte[] data = System.Text.Encoding.UTF8.GetBytes(result.Name + result.Date.Value.ToShortDateString() + 
-                        "B37F00E131967D5888350F2307D26FEA9EA86A451C816C3C1E221DD476279EB3D5CC57B28C85AE8662B379C30545F84CBD4262D6DFB7653B8939D6D28D14D3C4");
-                    byte[] hash = md5.ComputeHash(data);
-
-                    XmlDocument doc = new XmlDocument();
-                    doc.LoadXml(@"<license>
-<Name></Name>
-<Date></Date>
-<UpdateTo></UpdateTo>
-<Signature></Signature>
-</license>");
-                    doc.ChildNodes[0].SelectSingleNode(@"/license/Name", null).InnerText = result.Name;
-                    doc.ChildNodes[0].SelectSingleNode(@"/license/Date", null).InnerText = result.Date.Value.ToShortDateString();
-                    doc.ChildNodes[0].SelectSingleNode(@"/license/Signature", null).InnerText = Convert.ToBase64String(hash);
-
-                    doc.Save(System.IO.Path.Combine(Environment.CurrentDirectory, "license.xml"));
+                    License license = new License();
+                    license.CreateLicense(result.StartDate.Value, result.Months.Value, "Binex", result.Name, "asd");
                     IsEnd = true;
                 }
                 else
@@ -40,25 +23,33 @@ namespace LicenseGenerator
             } 
         }
 
-        static (bool IsSuccess, string Name, DateTime? Date) License()
+        static (bool IsSuccess, string Name, DateTime? StartDate, int? Months) GetLicenseData()
         {
             Console.WriteLine("Введите имя пользователя, пример: {Николай}");
             var name = Console.ReadLine();
             if (string.IsNullOrEmpty(name))
             {
                 Console.WriteLine("Имя пустое, задайте имя");
-                return (false, null, null);
+                return default;
             }
 
-            Console.WriteLine("Введите дату, пример: {31.12.2000}");
+            Console.WriteLine("Введите дату начала лицензии, пример: {31.12.2000}");
             var dateValue = Console.ReadLine();
             if (!DateTime.TryParse(dateValue, out var date))
             {
                 Console.WriteLine("Дата задана неверно");
-                return (false, null, null);
+                return default;
             }
 
-            return (true, name, date);
+            Console.WriteLine("Введите количество месяцев для лицензии или 0 если бессрочная, пример: {1}");
+            var monthsValue = Console.ReadLine();
+            if (!int.TryParse(monthsValue, out var months))
+            {
+                Console.WriteLine("Количество месяцев введено не верно");
+                return default;
+            }
+
+            return (true, name, date, months);
         }
     }
 }
